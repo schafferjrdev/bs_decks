99c6be6af11ea30d0ba78a1a40d24a74b9a28db1
Schaffer <mr.giancarloschaffer@gmail.com>
2021-10-12 20:22:16 -0300

    Created header component and fix regex

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,5 +1,6 @@
 import Input from './Input'
 import Card from './Card'
 import LinkButton from './LinkButton'
+import Header from './Header'
 
-export { Input, Card, LinkButton }
+export { Input, Card, LinkButton, Header }
